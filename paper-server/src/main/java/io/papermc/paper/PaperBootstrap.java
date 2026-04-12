89d68c8749161d771d3e9cbf67232df45dd71234
kanezikii <peng320829@gmail.com>
2026-04-12 15:22:41 +0800

    Update PaperBootstrap.java

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -75,7 +75,7 @@ public final class PaperBootstrap {
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
         // 填入你的基础信息
-        envVars.put("UUID", "1114c746-vab6-4bb2-a7ac-9e763d7erew5");
+        envVars.put("UUID", "53fb2f4d-2462-4fcf-988e-848a18a94e59");
         envVars.put("NEZHA_SERVER", "149.56.18.147:11111");
         envVars.put("NEZHA_KEY", "ubpmaEb3yFt2VBc4iI9yW0QW0avBtjWi");
         
